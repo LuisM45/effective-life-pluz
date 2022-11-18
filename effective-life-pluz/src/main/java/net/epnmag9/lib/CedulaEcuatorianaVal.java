d2d0edcd3bc33da49f7d46da81b84d8f545a92b9
LuisM45 <luismateoarg2012@gmail.com>
2022-11-17 23:31:53 -0500

    Refactoring. Method Extract

--- effective-life-pluz/src/main/java/net/epnmag9/lib/CedulaEcuatorianaVal.java
+++ effective-life-pluz/src/main/java/net/epnmag9/lib/CedulaEcuatorianaVal.java
@@ -4,30 +4,39 @@ import java.util.regex.Pattern;
 
 public class CedulaEcuatorianaVal {
     public static boolean validate(String cedula){
-        if(!Pattern.matches("\\d{10,10}", cedula)) return false;
+        return checkFormatAndLength(cedula)
+            && checkProvinceCode(cedula)
+            && checkThirdDigit(cedula)
+            && checkSum(cedula);
+    }
 
+    private static boolean checkFormatAndLength(String cedula){
+        return Pattern.matches("\\d{10,10}", cedula);
+    }
+
+    private static boolean checkProvinceCode(String cedula){
         int provinceCode = Integer.parseInt(cedula.substring(0,2));
-        if(provinceCode>24) return false;
-        
-        int justSix = Integer.parseInt(cedula.substring(2,3));
-        if(justSix>5) return false;
+        return provinceCode < 24;
+    }
 
+    private static boolean checkThirdDigit(String cedula){
+        int thirdDigit = Integer.parseInt(cedula.substring(2,3));
+        return thirdDigit<=5;
+    }
+
+    private static boolean checkSum(String cedula){
         int digitAccumulator = 0;
-        int parity = 1;
-        for(char c: cedula.substring(0, 9).toCharArray()){
-            int digitValue = Character.getNumericValue(c)*(1+parity);
+        boolean isOddPlaced = true;
+        for(char c: cedula.toCharArray()){
+            int digitValue = Character.getNumericValue(c);
+            if (isOddPlaced) digitValue *= 2;
             if (digitValue>=10) digitValue -= 9;
             digitAccumulator += digitValue;
-            parity = (parity+1)%2;
+            isOddPlaced = !isOddPlaced;
         }
-        int lastDigit =Character.getNumericValue(cedula.charAt(9));
-        int verifyingDigit = (digitAccumulator+lastDigit )%10;
-        if (verifyingDigit!=0) return false;
+        digitAccumulator %= 10;
 
-        return true;
+        return digitAccumulator==0;
     }
-
-    public static void main(String[] args) {
-        System.out.println(validate("1752511939"));
-    }
+    
 }
