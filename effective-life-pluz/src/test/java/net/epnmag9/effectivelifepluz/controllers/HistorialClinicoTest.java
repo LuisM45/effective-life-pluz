1204d126110af5f732e990de25d5957354784ce6
LuisM45 <luismateoarg2012@gmail.com>
2022-11-24 13:08:06 -0500

    Caso de prueba 1. @Test commit

--- effective-life-pluz/src/test/java/net/epnmag9/effectivelifepluz/controllers/HistorialClinicoTest.java
+++ effective-life-pluz/src/test/java/net/epnmag9/effectivelifepluz/controllers/HistorialClinicoTest.java
@@ -5,6 +5,9 @@
  */
 package net.epnmag9.effectivelifepluz.controllers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,15 +20,24 @@ import static org.junit.Assert.*;
  * @author Usuario
  */
 public class HistorialClinicoTest {
-    
+    static HistorialClinico historialClinico = null;
     public HistorialClinicoTest() {
     }
     
-    public static void given_two_dates_when_endDate_before_startDate_then_exception() {
-        
+    @Test(expected = IllegalArgumentException.class)
+    public void given_two_dates_when_endDate_before_startDate_then_exception() throws ParseException {
+        Date startDate = (new SimpleDateFormat("yyyyMMdd").parse("20000101"));
+        Date endDate = (new SimpleDateFormat("yyyyMMdd").parse("20000102"));
+        historialClinico.getEntradaInBetween(endDate,startDate);
     }
     @BeforeClass
-    public static void setUpClass() {
+    public static void setUpClass() throws ParseException {
+        historialClinico = new HistorialClinico();
+        SimpleDateFormat dateParser = new SimpleDateFormat("yyyyMMdd");
+        historialClinico.registerEntradaDatosClinicos(0, 0, 0, 0, dateParser.parse("20000101"), "");
+        historialClinico.registerEntradaDatosClinicos(0, 0, 0, 0, dateParser.parse("20010101"), "");
+        historialClinico.registerEntradaDatosClinicos(0, 0, 0, 0, dateParser.parse("20020101"), "");
+        historialClinico.registerEntradaDatosClinicos(0, 0, 0, 0, dateParser.parse("20030101"), "");
     }
     
     @AfterClass
